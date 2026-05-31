0d14aba88bd45e2ad3872beb1ca96460eabb82e3
marciasouzam <156952820+marciasouzam@users.noreply.github.com>
2026-05-31 23:43:41 +0000

    .

---
+++ implementation/ProdutoDAO.py
@@ -0,0 +1,8 @@
+from IConexaoDAO import IConexaoaDAO
+from IConexaoBD import IConexaoBD
+from models import Categoria
+from typing import Any
+
+class ConexaoDAO(IConexaoaDAO): #herdando a interface
+
+ 
